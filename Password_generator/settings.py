1e9fa56528b7a9763541de3a5130b9e6b084e673
Juan Fierro <juandafriveros87@gmail.com>
2023-07-27 11:46:40 -0500

    Atualizacion de pip a la ultima version

--- Password_generator/settings.py
+++ Password_generator/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'Password_generator.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postres:postgres@localhost:/postgres',
+        default='postgresql://postgres:postgres@localhost:/postgres',
         conn_max_age=600
     )
 }
